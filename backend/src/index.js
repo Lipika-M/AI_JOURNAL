08b9c30f66469969466e2f6e795368b192268be6
Lipika-M <lipikaam08@gmail.com>
2026-01-08 01:08:58 +0530

    Add cookie-parser and cors middleware; implement ApiError, ApiResponse, and asyncHandler utilities

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,16 @@
 import connectDB from "./db/index.js";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config({
-    path:'./.env'
-})
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT||3000,() => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    })
+  })
+  .catch((error) => {
+    console.log("MONGODB CONNECTION ERROR", error);
+  });
