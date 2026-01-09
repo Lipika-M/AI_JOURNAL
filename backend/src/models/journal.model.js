1fc4a696a15ea7452ea19ec8aa2bcf32c0755723
Lipika-M <lipikaam08@gmail.com>
2026-01-10 01:18:41 +0530

    Add user and journal models; update package.json with bcrypt and jsonwebtoken dependencies

---
+++ backend/src/models/journal.model.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+const journalSchema = new Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+      
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    sentiment: {
+      type: String,
+      enum: ["positive", "negative", "neutral"],
+      default: "neutral",
+      lowercase: true,
+      trim: true,
+    },
+    moodScore: {
+      type: Number,
+      min: -1,
+      max: 1,
+      default: 0,
+    },
+    summary: {
+      type: String,
+    },
+  tags: {
+  type: [String],
+  default: [],
+  set: tags => [...new Set(tags.map(t => t.toLowerCase().trim()))],
+},
+
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
