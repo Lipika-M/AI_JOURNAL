997b8beca34085998175444981d56fe79b719eef
Lipika-M <lipikaam08@gmail.com>
2026-03-03 21:43:50 +0530

    feat: refactor environment configuration and update server port to 5000, remove obsolete .env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,15 +1,12 @@
+import "dotenv/config";
 import connectDB from "./db/index.js";
-import dotenv from "dotenv";
 import {app} from "./app.js"
-
-dotenv.config({
-  path: "./.env",
-});
+ 
 
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT||3000,() => {
-      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    app.listen(process.env.PORT||5000,"0.0.0.0",() => {
+      console.log(`Server is running on port ${process.env.PORT || 5000}`);
     })
   })
   .catch((error) => {
